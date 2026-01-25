18d0e4f0198366dbd074f664a0209a9496b1288b
buj17 <igor.ssobolev.06@gmail.com>
2026-01-25 16:54:58 +0300

    path parameters

--- services/fastapi/main.py
+++ services/fastapi/main.py
@@ -1,8 +1,28 @@
+from enum import Enum
+
 from fastapi import FastAPI
+
+
+class MyModel(str, Enum):
+    val_1 = 'val_1'
+    val_2 = 'val_2'
+    val_3 = 'val_3'
 
 
 app = FastAPI()
 
-@app.get('/')
-async def index():
-    return {"hello": "world"}
+
+@app.get('/models/{model_name}')
+def get_model(model_name: MyModel):
+    if model_name is MyModel.val_1:
+        return {'model_name': model_name, 'message': 'Chosen 1'}
+
+    if model_name == 'val_2':
+        return {'model_name': model_name, 'message': 'Chosen 2 string literals'}
+
+    return {'model_name': model_name, 'message': 'Chosen 3 no variants'}
+
+
+@app.get('/files/{file_path:path}')
+async def read_file(file_path: str):
+    return {'file_path': file_path}

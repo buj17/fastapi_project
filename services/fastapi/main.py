dc92e9babc05ba13ad9572526eb0c14f40680a7e
buj17 <igor.ssobolev.06@gmail.com>
2026-02-01 16:41:19 +0300

    query params and string validations

--- services/fastapi/main.py
+++ services/fastapi/main.py
@@ -1,31 +1,29 @@
+import re
+from typing import Annotated, Pattern
+
 from fastapi import FastAPI
-from pydantic import BaseModel
-
-
-class Item(BaseModel):
-    name: str
-    description: str | None = None
-    price: float
-    tax: float | None = None
-
+from fastapi.params import Query
 
 app = FastAPI()
 
-
-@app.post('/items')
-async def create_item(item: Item):
-    item_dict = item.model_dump()
-    if item.tax is not None:
-        price_with_tax = item.price + item.tax
-        item_dict['price_with_tax'] = price_with_tax
-
-    return item_dict
+pattern: Pattern[str] | str = re.compile(r'^fixedquery$')
 
 
-@app.put('/items/{item_id}')
-async def update_item(item_id: int, item: Item, q: str | None = None):
-    response: dict[str, ...] = {**item.model_dump(), 'item_id': item_id}
-    if q is not None:
-        response['q'] = q
+@app.get('/items/')
+async def read_items(
+        q: Annotated[
+            str | None,
+            Query(
+                alias='item-query',
+                title='Query string',
+                description='Query string for the items to search in the database that have a good match',
+                min_length=3,
+                pattern=pattern,
+            ),
+        ] = None,
+):
+    results: dict[str, ...] = {'items': [{'item_id': 'Foo'}, {'item_id': 'Bar'}]}
+    if q:
+        results['q'] = q
 
-    return response
+    return results

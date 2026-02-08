98f022be4d21e44f8f41b87234b82da6ac2df01d
buj17 <igor.ssobolev.06@gmail.com>
2026-02-08 17:56:34 +0300

    Extra data types

--- services/fastapi/main.py
+++ services/fastapi/main.py
@@ -1,57 +1,29 @@
+import datetime
 from typing import Annotated
+from uuid import UUID
 
 import fastapi
 from fastapi import FastAPI
-from pydantic import (
-    BaseModel,
-    Field, HttpUrl,
-)
 
 app = FastAPI()
 
 
-class Image(BaseModel):
-    url: HttpUrl
-    name: str
-
-
-class Item(BaseModel):
-    name: Annotated[
-        str,
-        Field(
-            max_length=200
-        )
-    ]
-    description: Annotated[
-        str | None,
-        Field(
-            title='Item description',
-            max_length=300
-        )
-    ] = None
-    price: Annotated[
-        float,
-        Field(
-            gt=0,
-            description='The price must not be greater than zero'
-        )
-    ]
-    tax: float | None = None
-    tags: list[str] = []
-    images: list[Image] | None = None
-
-
-@app.put(
-    '/items/{item_id}',
-)
-async def update_item(
-    item_id: int,
-    item: Annotated[
-        Item,
-        fastapi.Body(
-            embed=True,
-        ),
-    ],
+@app.put('/items/{item_id}')
+async def read_items(
+    item_id: UUID,
+    start_datetime: Annotated[datetime.datetime, fastapi.Body()],
+    end_datetime: Annotated[datetime.datetime, fastapi.Body()],
+    process_after: Annotated[datetime.timedelta, fastapi.Body()],
+    repeat_at: Annotated[datetime.time | None, fastapi.Body()] = None
 ):
-    results = {'item_id': item_id, 'item': item}
-    return results
+    start_process = start_datetime + process_after
+    duration = end_datetime - start_process
+    return {
+        'item_id': item_id,
+        'start_datetime': start_datetime,
+        'end_datetime': end_datetime,
+        'process_after': process_after,
+        'repeat_at': repeat_at,
+        'start_process': start_process,
+        'duration': duration,
+    }

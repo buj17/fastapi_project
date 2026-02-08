35265e8d1b7772c819d8c9bfb3f5f747c6ac3ef1
buj17 <igor.ssobolev.06@gmail.com>
2026-02-08 17:38:46 +0300

    Body nested models

--- services/fastapi/main.py
+++ services/fastapi/main.py
@@ -4,28 +4,41 @@ import fastapi
 from fastapi import FastAPI
 from pydantic import (
     BaseModel,
-    Field,
+    Field, HttpUrl,
 )
 
 app = FastAPI()
 
 
-class Item(
-    BaseModel,
-):
-    name: str = Field(
-        max_length=200,
-    )
-    description: str | None = Field(
-        default=None,
-        title='Item description',
-        max_length=300,
-    )
-    price: float = Field(
-        gt=0,
-        description='The price must be grater than zero',
-    )
+class Image(BaseModel):
+    url: HttpUrl
+    name: str
+
+
+class Item(BaseModel):
+    name: Annotated[
+        str,
+        Field(
+            max_length=200
+        )
+    ]
+    description: Annotated[
+        str | None,
+        Field(
+            title='Item description',
+            max_length=300
+        )
+    ] = None
+    price: Annotated[
+        float,
+        Field(
+            gt=0,
+            description='The price must not be greater than zero'
+        )
+    ]
     tax: float | None = None
+    tags: list[str] = []
+    images: list[Image] | None = None
 
 
 @app.put(
